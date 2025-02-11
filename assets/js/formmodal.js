acbfe5dc789168bdb04174f3f09eece73744be87
Dhani <110752076+amd119@users.noreply.github.com>
2025-02-11 14:44:33 +0700

    Update: Form Modal

---
+++ assets/js/formmodal.js
@@ -0,0 +1,99 @@
+function openIndomaretForm() {
+    const firstModal = document.getElementById('indomaretModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#indomaretForm');
+      secondModal.show();
+  }
+function openAdvanForm() {
+    const firstModal = document.getElementById('advanModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#advanForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openPharosForm() {
+    const firstModal = document.getElementById('pharosModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#pharosForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openTridayaForm() {
+    const firstModal = document.getElementById('tridayaModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#tridayaForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openIndopsikoForm() {
+    const firstModal = document.getElementById('indopsikoModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#indopsikoForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openPkkdForm() {
+    const firstModal = document.getElementById('pkkdModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#pkkdForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openTriadiForm() {
+    const firstModal = document.getElementById('triadiModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#triadiForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openSigmaForm() {
+    const firstModal = document.getElementById('sigmaModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#sigmaForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openVillaMerahForm() {
+    const firstModal = document.getElementById('villaMerahModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#villaMerahForm');
+      secondModal.show();
+    }, {once: true});
+  }
+  function openSutindoForm() {
+    const firstModal = document.getElementById('sutindoModal');
+    const modalInstance = bootstrap.Modal.getInstance(firstModal);
+    modalInstance.hide();
+  
+    firstModal.addEventListener('hidden.bs.modal', function() {
+      const secondModal = new bootstrap.Modal('#sutindoForm');
+      secondModal.show();
+    }, {once: true});
+  }
